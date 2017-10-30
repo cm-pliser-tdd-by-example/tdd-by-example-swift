f99453cbe52d318306764901215b66455257e24d
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2017-10-30 11:12:30 +0900

    [add] 雛形としてトランプカードのテストを追加

---
+++ tdd-by-example-swift/Card.swift
@@ -0,0 +1,8 @@
+//
+//  CardSpec.swift
+//  tdd-by-example-swift
+//
+//  Copyright © 2017年 cm-pliser. All rights reserved.
+//
+
+import Foundation
---
+++ tdd-by-example-swiftTests/CardSpec.swift
@@ -0,0 +1,23 @@
+//
+//  CardSpec.swift
+//  tdd-by-example-swiftTests
+//
+//  Copyright © 2017年 cm-pliser. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import tdd_by_example_swift
+
+class CardSpec: QuickSpec {
+    
+    override func spec() {
+        describe("") {
+            context("") {
+                it("") {
+                    
+                }
+            }
+        }
+    }
+}
