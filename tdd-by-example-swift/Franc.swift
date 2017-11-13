90a595528d8c81d224911a75b32a1da26d83bd52
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2017-11-13 19:56:19 +0900

    ソースを戻して、一度グリーンまで巻き戻す。

--- tdd-by-example-swift/Franc.swift
+++ tdd-by-example-swift/Franc.swift
@@ -17,6 +17,6 @@ class Franc: Money {
     override func times(_ multiplier: Int) -> Money {
         // 本の中では一度 return null してから実装を修正するステップを踏んでいるが、
         // Swiftでnullを扱うにはOptionalがあるため、直接記述する。
-        return Money(amount * multiplier, currency)
+        return Franc(amount * multiplier, currency)
     }
 }
