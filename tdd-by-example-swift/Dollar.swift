3c9d35e85f3b9fa3ef9f359ce2c36065e30fa3cd
nakayasu.yuichi <nakayasu.yuichi@classmethod.jp>
2017-11-13 19:39:43 +0900

    timesメソッドの中でインスタンス変数を渡すように変更

--- tdd-by-example-swift/Dollar.swift
+++ tdd-by-example-swift/Dollar.swift
@@ -16,6 +16,6 @@ class Dollar: Money {
     override func times(_ multiplier: Int) -> Money {
         // 本の中では一度 return null してから実装を修正するステップを踏んでいるが、
         // Swiftでnullを扱うにはOptionalがあるため、直接記述する。
-        return Dollar(amount * multiplier, "USD")
+        return Dollar(amount * multiplier, currency)
     }
 }
